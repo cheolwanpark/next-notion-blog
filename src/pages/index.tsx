64c69e0acc2cfcd4831b8122eb92ac3d4210531d
CodingVillain <cheolwan.park552@gmail.com>
2023-03-06 11:01:53 +0000

    rm: unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Intro } from "@/components/intro";
 import { Posts } from "@/components/posts";
-import { isServer } from "@/services/is_server";
 import { query } from "@/services/notion/query";
 import { QueryResponse } from "@/services/notion/types";
 import { GetServerSideProps } from "next";
