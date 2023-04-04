6f3aafec7a4d6072ce42267e359ba5f78e4543e3
CodingVillain <cheolwan.park552@gmail.com>
2023-04-04 05:45:28 +0000

    add: open graph image generated by @vercel/og

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -10,19 +10,25 @@ export const MetaHead = ({
   description?: string;
   url?: string;
 }) => {
+  title = title || config.blogTitle;
+  description = description || config.defaultSiteDescription;
+  url = url || config.baseURL;
+  const ogImageURL = `${config.baseURL}/api/og${
+    title === config.blogTitle ? "" : `?title=${title}`
+  }`;
   return (
     <Head>
-      <title>{title || config.blogTitle}</title>
-      <meta
-        name="description"
-        content={description || config.defaultSiteDescription}
-      />
+      <title>{title}</title>
+      <meta name="description" content={description} />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:title" content={title || config.blogTitle} />
+      <meta property="og:title" content={title} />
       <meta property="og:site_name" content={config.blogTitle} />
       <meta property="og:type" content="blog" />
-      <meta property="og:url" content={url || config.baseURL} />
+      <meta property="og:url" content={url} />
       <meta property="og:article:author" content={config.owner} />
+      <meta property="og:image" content={ogImageURL} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Head>
   );
 };
