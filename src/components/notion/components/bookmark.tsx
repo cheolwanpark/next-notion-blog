d05afb29087d386ec2e99077767cfd9c34545bea
CodingVillain <cheolwan.park552@gmail.com>
2023-04-05 10:44:28 +0000

    fix: bookmark block design

--- src/components/notion/components/bookmark.tsx
+++ src/components/notion/components/bookmark.tsx
@@ -19,7 +19,15 @@ export const Bookmark = ({
     <div className={styles.bookmark_box}>
       <div className={styles.bookmark}>
         <div className={styles.informations}>
-          {metadata.title && <h1>{metadata.title}</h1>}
+          <div className={styles.title}>
+            {metadata.icon && (
+              <img
+                src={metadata.icon}
+                alt={`${metadata.title ? metadata.title : metadata.url}'s icon`}
+              />
+            )}
+            {metadata.title && <h1>{metadata.title}</h1>}
+          </div>
           {metadata.description && <h2>{metadata.description}</h2>}
           {metadata.url && <h3>{metadata.url}</h3>}
         </div>
