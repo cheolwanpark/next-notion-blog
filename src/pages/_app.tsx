e5bcb0b80c7c30df939b489695946d0987db50b7
CodingVillain <cheolwan.park552@gmail.com>
2023-04-07 08:28:13 +0000

    fix: use data-theme in body only

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,20 +9,21 @@ import { Footer } from "@/components/footer";
 import "@/services/dayjs";
 import { Analytics } from "@vercel/analytics/react";
 
-const ITEM_NAME = "mode";
-const DARK = "dark";
-const LIGHT = "light";
-
 export default function App({ Component, pageProps }: AppProps) {
   const [isDarkMode, _setMode] = useState<boolean | null>(null);
 
   const setMode = (val: boolean) => {
     _setMode(val);
-    localStorage.setItem(ITEM_NAME, val ? DARK : LIGHT);
+    const theme = val ? "dark" : "light";
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("mode", theme);
   };
 
   useEffect(() => {
-    setMode(haveSetDarkMode());
+    const bodyTheme = document.body.getAttribute("data-theme");
+    const isBodyDarkTheme = bodyTheme !== null && bodyTheme === "dark";
+    setMode(isBodyDarkTheme);
+
     window
       .matchMedia("(prefers-color-scheme: dark)")
       .addEventListener("change", (event) => {
@@ -32,33 +33,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <DarkModeContext.Provider value={{ isDarkMode, setMode }}>
-      <div
-        className="screen"
-        data-theme={
-          isDarkMode !== null ? (isDarkMode ? DARK : LIGHT) : undefined
-        }
-      >
-        <Navigation />
-        <main className="container main">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </div>
+      <Navigation />
+      <main className="container main">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
       <Analytics />
     </DarkModeContext.Provider>
   );
 }
-
-const haveSetDarkMode = () => {
-  if (!localStorage.getItem(ITEM_NAME)) {
-    return isPreferColorSchemeIsDark();
-  }
-  return localStorage.getItem(ITEM_NAME) === DARK;
-};
-
-const isPreferColorSchemeIsDark = () => {
-  return (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  );
-};
