faa65b5a6bb89ac5a509060c86fad2de5fffef54
CodingVillain <cheolwan.park552@gmail.com>
2023-04-14 11:23:08 +0000

    fix: remove revalidate page with short revalidate time, revalidate page is not needed

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ const siteConfig = {
   blogTitle: "codingvillain",
   github: "Cheolwan-Park",
   databseID: "392acd2f7b5849e784cc4b0009aeabf5",
-  revalidateTime: 30 * 60,
+  revalidateTime: 15 * 60,
   optimizeExpiringImages: false,
   postsPerPage: 5,
   previewPosts: 3,
